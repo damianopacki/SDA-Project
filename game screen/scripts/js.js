382a5c3c0d6321db59867ff17c05b31b37fa147c
Damian Opacki <damian.opacki@gmail.com>
2017-09-03 20:20:01 +0200

    changed approach to object oriented

--- game screen/scripts/js.js
+++ game screen/scripts/js.js
@@ -1,77 +1,140 @@
-const easyArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
-let memoryValues = [];
-let tileIds = [];
-let tilesReverted = 0;
+// const easyArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+// let memoryValues = [];
+// let tileIds = [];
+// let tilesReverted = 0;
 
+// function shuffleTiles (array) {
+//     return array.sort(() => Math.random() - 0.5);
+//   }
 
-// Array.prototype.shuffleTiles = function() {
-//     let i = this.length, j, temp;
-//     while(--i > 0) {
-//         j = Math.floor(Math.random() * (i + 1));
-//         temp = this[j];
-//         this[j] = this[i];
-//         this[i] = temp;
-//     } 
+// function newGameBoard(array) {
+//     let output = '';
+//     console.log(easyArray);
+//     shuffleTiles(easyArray);
+//     for (let i = 0; i < easyArray.length; i++) {
+//         output += '<div id="title'+i+'" onclick="revertTile(this, \''+easyArray[i]+'\')"></div>';
+//     }
+//     console.log(easyArray);
+    
+    
+//     document.getElementById('gameBoard').innerHTML = output;
 // }
 
-function shuffleTiles (arr) {
-    return arr.sort(() => Math.random() - 0.5);
-  }
+// window.onload = newGameBoard();
 
-function newGameBoard(array) {
-    let output = '';
-    console.log(easyArray);
-    shuffleTiles(easyArray);
-    for (let i = 0; i < easyArray.length; i++) {
-        output += '<div id="title'+i+'" onclick="revertTile(this, \''+easyArray[i]+'\')"></div>';
-    }
-    console.log(easyArray);
-    
-    
-    document.getElementById('gameBoard').innerHTML = output;
-}
+// function revertTile(tile, val) {
+//     if (tile.innerHTML === "" && memoryValues.length < 2) {
+//         tile.style.background = '#FFF';
+//         tile.innerHTML = val;
+//         if (memoryValues.length === 0) {
+//             memoryValues.push(val);
+//             tileIds.push(tile.id);
+//         } 
+//         else if (memoryValues.length === 1) {
+//             memoryValues.push(val);
+//             tileIds.push(tile.id);
+//             if (memoryValues[0] === memoryValues[1]) {
+//                 tilesReverted += 2;
+//                 // Clear both arrays
+//                 memoryValues = [];
+//                 tileIds = [];
+//                 // Check to see if the whole board is cleared
+//                 if (tilesReverted === easyArray.length) {
+//                     alert('board cleared... generating new board');
+//                     document.getElementById('gameBoard').innerHTML = '';
+//                     newGameBoard();
+//                 }
+//             } else {
+//                 setTimeout(revertTileBack, 700);
+//             }
+//         }
 
-window.onload = newGameBoard();
+//     }
+// }
 
-function revertTile(tile, val) {
-    if (tile.innerHTML === "" && memoryValues.length < 2) {
-        tile.style.background = '#FFF';
-        tile.innerHTML = val;
-        if (memoryValues.length === 0) {
-            memoryValues.push(val);
-            tileIds.push(tile.id);
-        } 
-        else if (memoryValues.length === 1) {
-            memoryValues.push(val);
-            tileIds.push(tile.id);
-            if (memoryValues[0] === memoryValues[1]) {
-                tilesReverted += 2;
-                // Clear both arrays
-                memoryValues = [];
-                tileIds = [];
-                // Check to see if the whole board is cleared
-                if (tilesReverted === easyArray.length) {
-                    alert('board cleared... generating new board');
-                    document.getElementById('gameBoard').innerHTML = '';
-                    newGameBoard();
-                }
-            } else {
-                setTimeout(revertTileBack, 700);
-            }
+// function revertTileBack() {
+//     // Flip the 2 tiles back over 
+//     const tile1 = document.getElementById(tileIds[0]);
+//     const tile2 = document.getElementById(tileIds[1]);
+//     tile1.style.background = 'yellow';
+//     tile1.innerHTML = "";
+//     tile2.style.background = 'yellow';
+//     tile2.innerHTML = "";
+//     // Clear both arrays
+//     memoryValues = [];
+//     tileIds = [];
+// }
+
+
+
+
+const memoryGame = {
+
+    easyArray: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'],
+    memoryValues: [],
+    tileIds: [],
+    tilesReverted: 0,
+
+    init: function() {
+        memoryGame.newGameBoard(memoryGame.easyArray);
+    },
+
+    shuffleTiles: function(array) {
+        return array.sort(() => Math.random() - 0.5);
+    },
+    newGameBoard: function(array) {
+        let output = '';
+        console.log(memoryGame.easyArray);
+        memoryGame.shuffleTiles(array);
+        for (let i = 0; i < array.length; i++) {
+            output += '<div id="title'+i+'" onclick="memoryGame.revertTile(this, \''+ array[i] +'\')"></div>';
         }
+        console.log(output);
+        console.log(array);
+        
+        $('#gameBoard').html(output);
+    },
+    revertTile: function(tile, val) {
+        if (tile.innerHTML === "" && memoryGame.memoryValues.length < 2) {
+            tile.style.background = '#FFF';
+            tile.innerHTML = val;
+            if (memoryGame.memoryValues.length === 0) {
+                memoryGame.memoryValues.push(val);
+                memoryGame.tileIds.push(tile.id);
+            } 
+            else if (memoryGame.memoryValues.length === 1) {
+                memoryGame.memoryValues.push(val);
+                memoryGame.tileIds.push(tile.id);
+                if (memoryGame.memoryValues[0] === memoryGame.memoryValues[1]) {
+                    memoryGame.tilesReverted += 2;
+                    // Clear both arrays
+                    memoryGame.memoryValues = [];
+                    memoryGame.tileIds = [];
+                    // Check to see if the whole board is cleared
+                    if (memoryGame.tilesReverted === memoryGame.easyArray.length) {
+                        alert('board cleared... generating new board');
+                        $('#gameBoard').html('');
+                        memoryGame.newGameBoard(memoryGame.easyArray);
+                    }
+                } else {
+                    setTimeout(memoryGame.revertTileBack, 700);
+                }
+            }
 
-    }
+        }
+    }, 
+    revertTileBack: function() {
+        // Flip the 2 tiles back over 
+        const tile1 = document.getElementById(memoryGame.tileIds[0]);
+        const tile2 = document.getElementById(memoryGame.tileIds[1]);
+        tile1.style.background = 'yellow';
+        tile1.innerHTML = "";
+        tile2.style.background = 'yellow';
+        tile2.innerHTML = "";
+        // Clear both arrays
+        memoryGame.memoryValues = [];
+        memoryGame.tileIds = [];
+    },
 }
 
-function revertTileBack() {
-    // Flip the 2 tiles back over 
-    const tile1 = document.getElementById(tileIds[0]);
-    const tile2 = document.getElementById(tileIds[1]);
-    tile1.style.background = 'yellow';
-    tile1.innerHTML = "";
-    tile2.style.background = 'yellow';
-    tile2.innerHTML = "";
-    // Clear both arrays
-    memoryValues = [];
-    tileIds = [];
-}
+memoryGame.init();
