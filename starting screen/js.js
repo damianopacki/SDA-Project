82e7eaa2d5e52f70e0f068cce1e4738955215cb6
damianop <damian.opacki@gmail.com>
2017-09-04 20:52:50 +0200

    fixed setting game difficulty

--- starting screen/js.js
+++ starting screen/js.js
@@ -2,3 +2,5 @@ $('#difficulties').change(function(){
     localStorage.setItem('difficulty', $(this).val());
     console.log($(this).val());
   });
+
+$('#difficulties').val(localStorage.getItem('difficulty'));
